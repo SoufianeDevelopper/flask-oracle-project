a18bb1c408f4a3709bec39577c6aafa4df7a664d
soufiane <soufiane.khalfouna@gmail.com>
2026-06-15 19:19:34 +0000

    edit fichier app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def index():
         
         return jsonify({
             "status": "Success",
-            "message": f"hello mahbol Connecté avec succès à la base Oracle : {db_name}",
+            "message": f"hello lahbil  Connecté avec succès à la base Oracle : {db_name}",
             "host": "VM-Flask-Server"
         })
     except Exception as e:
